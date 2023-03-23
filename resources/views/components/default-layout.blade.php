76107724755f3bef4669fec7e5ab646f4c6b449a
Elkana Vinet <elkana.dimbiniaina@hotmail.com>
2023-03-23 19:26:48 +0300

    Setting up blog basic blog homepage

--- resources/views/components/default-layout.blade.php
+++ resources/views/components/default-layout.blade.php
@@ -3,74 +3,65 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="@lang('meta.keywords')">
+    <meta name="description" content="@lang('meta.description')">
+    <meta name="author" content="@lang('meta.author')">
     @vite('resources/css/app.css')
     <title>{{ env('APP_NAME') }}</title>
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9832408140141934"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-<header class="bg-blue-200">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1">
-            <a href="#" class="-m-1.5 p-1.5">
-                <span class="sr-only">Elkana Vinet</span>
-                <img class="h-8 w-auto" src="{{ asset('assets/img/mini-logo.png') }}" alt="">
-            </a>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('assets/img/mini-logo.png') }}"
+                                                                class="img-fluid"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">@lang('menu.home')</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">@lang('menu.tutorials')</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">@lang('menu.contact')</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="@lang('search.search')"
+                       aria-label="@lang('search.search')">
+                <button class="btn btn-primary" type="submit">@lang('search.search')</button>
+            </form>
         </div>
-        <div class="flex lg:hidden">
-            <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                <span class="sr-only">Open main menu</span>
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-            </button>
-        </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-            <div class="relative">
-                <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-                    Accueil
-                </button>
-            </div>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">@lang('sidebar.last_article')</h5>
 
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Tutoriels</a>
-        </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Se connecter <span aria-hidden="true">&rarr;</span></a>
-        </div>
-    </nav>
-    <!-- Mobile menu, show/hide based on menu open state. -->
-    <div class="lg:hidden" role="dialog" aria-modal="true">
-        <!-- Background backdrop, show/hide based on slide-over state. -->
-        <div class="fixed inset-0 z-10"></div>
-        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Elkana Vinet</span>
-                    <img class="h-8 w-auto" src="{{ asset('assets/img/mini-logo.png') }}" alt="">
-                </a>
-                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-                    <span class="sr-only">Fermer</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-            <div class="mt-6 flow-root">
-                <div class="-my-6 divide-y divide-gray-500/10">
-                    <div class="space-y-2 py-6">
-                        <div class="-mx-3">
-                            <button type="button" class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50" aria-controls="disclosure-1" aria-expanded="false">
-                                Accueil
-                            </button>
-                        </div>
-                        <a href="#" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Tutoriels</a>
-                    </div>
-                    <div class="py-6">
-                        <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Se connecter</a>
-                    </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-10">
+            {{ $slot }}
+        </div>
     </div>
-</header>
+</div>
+
+<footer class="">
+    <div class="text-center">
+        <small class="text-center">Copyright © 2022 <a target="_blank" href="https://www.elkana-vinet.com">Elkana
+                Vinet</a> | Crédits photos : <a target="_blank" href="https://www.pexels.com">Pexels</a></small>
+    </div>
+</footer>
+@vite('resources/js/app.js')
 </body>
 </html>
