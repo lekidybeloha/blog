842d6f86ccf900ce086b39b4ee49d8de463c7900
Elkana Vinet <elkana.dimbiniaina@hotmail.com>
2023-03-24 09:16:31 +0300

    Create login and attempt workflow

--- resources/views/administrations/login.blade.php
+++ resources/views/administrations/login.blade.php
@@ -12,16 +12,22 @@
         <div class="col-md-auto">
             <div class="card p-3">
                 <div class="card-body">
-                    <h5 class="card-title">Administration - Se connecter</h5>
+                    <h5 class="card-title text-center">Administration - Se connecter</h5>
                     <form method="POST" class="mt-5">
-                        <input type="text" name="username" placeholder="Nom d'utilisateur" class="form-control" required>
+                        @if($errors->any())
+                            @foreach($errors->all() as $error)
+                                <div class="alert alert-danger" role="alert">
+                                    {!! $error !!}
+                                </div>
+                            @endforeach
+                        @endif
+                        <input type="text" name="username" placeholder="Nom d'utilisateur" class="form-control" value="{{ old('username') }}" required>
                         <input type="password" name="password" placeholder="Mot de passe" class="form-control mt-3" required>
                         <input type="checkbox" class="form-check-input mt-2" id="remember">
                         <label class="form-check-label mt-2" for="remember">Se souvenir de moi</label>
                         <div class="text-center">
                             <button type="submit" class="btn btn-primary mt-3">Se connecter</button>
                         </div>
-
                         @csrf
                     </form>
                 </div>
--- routes/administrations.php
+++ routes/administrations.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\Administrations\AdministrationController;
+use App\Http\Controllers\Auth\Administrations\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AdministrationController::class, 'login'])->name('admin-login');
+//Auth route for administrations
+Route::get('/login', [AuthController::class, 'login'])->name('admin-login');
+Route::post('/login', [AuthController::class, 'attempt'])->name('admin-attempt');
+
+//Dashboard administrations
+Route::get('/dashboard', [AdministrationController::class, 'index'])->middleware(['auth:administrators'])->name('admin-dashboard');
