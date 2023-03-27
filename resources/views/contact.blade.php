31b6d3c1a1eae4360ab81c802959a68a2b296e85
Elkana Vinet <elkana.dimbiniaina@hotmail.com>
2023-03-27 16:29:21 +0300

    add name field in contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,15 +4,15 @@
         <form id="contact-form" method="POST">
             <div class="mb-3">
                 <label for="name" class="form-label">Nom</label>
-                <input type="text" class="form-control" id="name" placeholder="Nom" required>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Nom" required>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Email" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Message</label>
-                <textarea class="form-control" id="message" placeholder="Message" required></textarea>
+                <textarea class="form-control" id="message" name="message" placeholder="Message" required></textarea>
             </div>
             <div>
                 <button
